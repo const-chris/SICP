93dfe9860633637128aaa4094a10a5f5d7c3afa1
Chris <30579663+GHDIShfdiod876dhs@users.noreply.github.com>
2020-01-10 18:01:05 -0700

    finish 3.36

--- 3.36.rkt
+++ 3.36.rkt
@@ -74,3 +74,81 @@ global --> |  a --+     make-connector --+                     b --+            
 |#
 
 (set-value! a 10 'user)
+#|
+           +---------------------------------------------------------------------------------------------------+
+           |                                                                                                   |
+global --> |  a --+     make-connector --+                                                b --+                |
+           |      |                      |                                                    |                |
+           +------|----------------------|----------------------------------------------------|----------------+
+                  |         ^            |   ^                                                |         ^
+                  |         |            v   |                                                |         |
+                  |         |           @=@--+                                                |         |
+                  |    +---------+      |                                                     |    +---------+
+                  |    |         |      |                                                     |    |         |
+                  |    +---------+      v                                                     |    +---------+
+                  |         ^         params: _                                               |         ^
+                  |         |         body: (let ((value ... )))                              |         |
+                  |         |                                                                 |         |
+                  |    +------------------------+                                             |    +------------------------+
+                  |    | value: false           |                                             |    | value: false           |
+                  |    | informant: false       |<----------+                                 |    | informant: false       |
+                  |    | constraints: '()       |           |                                 |    | constraints: '()       |
+                  |    | set-my-value: -----------------+   |                                 |    | set-my-value: ...      |
+                  |    | forget-my-value: ...   |       |   |                                 |    | forget-my-value: ...   |
+                  |    | connect: ...           |       |   |                                 |    | connect: ...           |
+                  | +--- me                     |       |   |                                 | +--- me                     |
+                  | |  +------------------------+       |   |                                 | |  +------------------------+
+                  | |       ^          ^                |   |                                 | |       ^
+                  | |       |          |                v   |                                 | |       |
+                  v v       |          |               @=@--+                                 v v       |
+                  @=@-------+   +---------------+      |                                      @=@-------+
+                  |             | newval: 10    |      |                                      |
+                  |             | setter: 'user |      v                                      |
+                  |             +---------------+    params: newval, setter                   |
+                  |                                  body: (cond ...                          |
+                  v                                              (for-each-except ... ))      v
+                params: request                                                             params: request
+                body: (cond ((eq? request 'has-value?) ... ))                               body: (cond ((eq? request 'has-value?) ... ))
+
+
+
+
+
+
+
+           +---------------------------------------------------------------------------------------------------------+
+           |                                                                                                         |
+global --> |  a --+     make-connector --+         for-each-except: ...                         b --+                |
+           |      |                      |                                                          |                |
+           +------|----------------------|----------------------------------------------------------|----------------+
+                  |         ^            |   ^                      ^                               |         ^
+                  |         |            v   |                      |                               |         |
+                  |         |           @=@--+                      |                               |         |
+                  |    +---------+      |                     +--------------------------------+    |    +---------+
+                  |    |         |      |                     | exception: 'user               |    |    |         |
+                  |    +---------+      |                     | procedure: inform-about-value  |    |    +---------+
+                  |         ^           v                     | list: '()                      |    |         ^
+                  |         |         params: _               +--------------------------------+    |         |
+                  |         |         body: (let ((value ... )))                                    |         |
+                  |         |                                                                       |         |
+                  |    +------------------------+                                                   |    +------------------------+
+                  |    | value: 10              |                                                   |    | value: false           |
+                  |    | informant: 'user       |<----------+                                       |    | informant: false       |
+                  |    | constraints: '()       |           |                                       |    | constraints: '()       |
+                  |    | set-my-value: -----------------+   |                                       |    | set-my-value: ...      |
+                  |    | forget-my-value: ...   |       |   |                                       |    | forget-my-value: ...   |
+                  |    | connect: ...           |       |   |                                       |    | connect: ...           |
+                  | +--- me                     |       |   |                                       | +--- me                     |
+                  | |  +------------------------+       |   |                                       | |  +------------------------+
+                  | |       ^          ^                |   |                                       | |       ^
+                  | |       |          |                v   |                                       | |       |
+                  v v       |          |               @=@--+                                       v v       |
+                  @=@-------+   +---------------+      |                                            @=@-------+
+                  |             | newval: 10    |      |                                            |
+                  |             | setter: 'user |      v                                            |
+                  |             +---------------+    params: newval, setter                         |
+                  |                                  body: (cond ...                                |
+                  v                                              (for-each-except ... ))            v
+                params: request                                                             params: request
+                body: (cond ((eq? request 'has-value?) ... ))                               body: (cond ((eq? request 'has-value?) ... ))
+|#
