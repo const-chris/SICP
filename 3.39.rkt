d0b3f0424e2e9691dbe21def26a443f1eaf2820b
Chris <30579663+GHDIShfdiod876dhs@users.noreply.github.com>
2020-01-16 13:41:14 -0700

    formatting

--- 3.39.rkt
+++ 3.39.rkt
@@ -2,7 +2,9 @@
 ;; REVISIT -- test with serializer implementation
 
 (define x 10)
+
 (define s (make-serializer))
+
 (parallel-execute
  (lambda () (set! x ((s (lambda () (* x x))))))
  (s (lambda () (set! x (+ x 1)))))
