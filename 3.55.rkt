cbd5996782eb1a76d794c8b4ed4ae6c78f9f4072
Chris <30579663+GHDIShfdiod876dhs@users.noreply.github.com>
2020-01-22 11:06:28 -0700

    export partial-sums

--- 3.55.rkt
+++ 3.55.rkt
@@ -1,5 +1,6 @@
 #lang sicp
 (#%require (file "stream-utils.rkt"))
+(#%provide partial-sums)
 
 (define (partial-sums s)
   (cons-stream (stream-car s)
@@ -7,4 +8,6 @@
 
 
 ;; test
+#|
 (stream-take 10 (partial-sums integers))
+;|#
