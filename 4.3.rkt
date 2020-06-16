ae5bd260cceb005b2f6c040c86f63b60b11d9d4b
Chris <30579663+GHDIShfdiod876dhs@users.noreply.github.com>
2020-06-16 14:27:13 -0600

    implement functioning data-directed version of interpreter
    table available in data-directed-table.rkt

--- 4.3.rkt
+++ 4.3.rkt
@@ -1,21 +1,13 @@
 #lang sicp
+(#%require (file "./data-directed-table.rkt"))
+(#%require (file "./interpreter.rkt"))
+(#%provide (all-from (file "./data-directed-table.rkt"))
+           (all-from (file "./interpreter.rkt"))
+           (all-defined))
 
-(define (install-eval-procedures)
-  (put 'eval 'quote  (lambda (exp env) (text-of-quotation exp)))
-  (put 'eval 'set!   (lambda (exp env) (eval-assignment exp env)))
-  (put 'eval 'define (lambda (exp env) (eval-definition exp env)))
-  (put 'eval 'if     (lambda (exp env) (eval-if exp env)))
-  (put 'eval 'lambda (lambda (exp env) (make-procedure (lambda-parameters exp) (lambda-body exp) env)))
-  (put 'eval 'begin  (lambda (exp env) (eval-sequence (begin-actions exp) env)))
-  (put 'eval 'cond   (lambda (exp env) (eval (cond->if exp) env))))
-
-
-(install-eval-procedures)
-
-
+;; -------------------- new eval --------------------
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
-        ((variable? exp) (lookup-variable-value exp env))
         ((get 'eval (exp-type exp)) => (lambda (f) (f exp env)))
         ((application? exp)
          (apply (eval (operator exp) env)
@@ -23,5 +15,80 @@
         (else
          (error "Unknown expression type: EVAL" exp))))
 
+(define (exp-type exp)
+  (if (variable? exp)
+      'variable
+      (car exp)))
 
-(define (exp-type exp) (car exp))
+;; -------------------- unchanged --------------------
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+          (procedure-body procedure)
+          (extend-environment
+           (procedure-parameters procedure)
+           arguments
+           (procedure-environment procedure))))
+        (else
+         (error
+          "Unknown procedure type: APPLY" procedure))))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps)
+         (eval (first-exp exps) env))
+        (else
+         (eval (first-exp exps) env)
+         (eval-sequence (rest-exps exps) env))))
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval (assignment-value exp) env)
+                       env)
+  'ok)
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+    (eval (definition-value exp) env)
+    env)
+  'ok)
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+;; ----------- eval procedures package -----------
+(define (install-eval-procedures)
+  (display "installing 'eval procedures... ")
+  (put 'eval 'variable (lambda (exp env) (lookup-variable-value exp env)))
+  (put 'eval 'quote    (lambda (exp env) (text-of-quotation exp)))
+  (put 'eval 'set!     (lambda (exp env) (eval-assignment exp env)))
+  (put 'eval 'define   (lambda (exp env) (eval-definition exp env)))
+  (put 'eval 'if       (lambda (exp env) (eval-if exp env)))
+  (put 'eval 'lambda   (lambda (exp env) (make-procedure (lambda-parameters exp) (lambda-body exp) env)))
+  (put 'eval 'begin    (lambda (exp env) (eval-sequence (begin-actions exp) env)))
+  (put 'eval 'cond     (lambda (exp env) (eval (cond->if exp) env))))
+
+(install-eval-procedures)
+
+#| ------------------- tests -------------------
+(eval '1 the-global-environment)
+(eval 'car the-global-environment)
+;|#
+
---
+++ data-directed-table.rkt
@@ -0,0 +1,37 @@
+#lang sicp
+(#%provide get put)
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+              (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                    (assoc key-2 (cdr subtable))))
+              (if record (cdr record) false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable
+              (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                    (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup) lookup)
+            ((eq? m 'insert!) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+
+(define table (make-table))
+(define get (table 'lookup))
+(define put (table 'insert!))
