ebaba5694775c19a4b0302a707d464eab7a410cb
Chris <30579663+GHDIShfdiod876dhs@users.noreply.github.com>
2020-07-13 17:27:04 -0600

    add stream-append to stream-utils

--- stream-utils.rkt
+++ stream-utils.rkt
@@ -75,3 +75,9 @@
       s2
       (cons-stream (stream-car s1)
                    (interleave s2 (stream-cdr s1)))))
+
+(define (stream-append s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (stream-append (stream-cdr s1) s2))))
