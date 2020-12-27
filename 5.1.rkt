8239916721a609a3db3444c936b640f26f344786
Chris <30579663+GHDIShfdiod876dhs@users.noreply.github.com>
2020-12-27 01:49:14 -0500

    5.1

---
+++ 5.1.rkt
@@ -0,0 +1,59 @@
+#| Exercise 5.1: Design a register machine to compute factorials using the iterative algorithm specified by the following |#
+#| procedure. Draw data-path and controller diagrams for this |#
+#| machine. |#
+
+(define (factorial n)
+  (define (iter product counter)
+    (if (> counter n)
+        product
+        (iter (* counter product)
+              (+ counter 1))))
+  (iter 1 1))
+
+
+; Data Path:
+
+        p<-m  ┌───────┐      p<-1
+     ┌──Ⓧ────>│   p   │<─────Ⓧ─────┐
+     │        └───────┘            │
+     │            │                v
+  ───────         │              ────
+ ╱   *   ╲        │        ┌─────╲1 ╱───┐
+╱─────────╲       │        │      ╲╱    │
+  ^     ^         │        │            │
+  │     │         │        Ⓧ c<-1       │
+  │     └─────────┘        │            │
+  │                        │            │
+  │           ┌───────┐    │            V
+  └───────────│   c   │<───┘       ╲─────────╱
+              └───────┘             ╲   +   ╱
+               │     ^               ╲─────╱
+               │     │     c<-a         │
+┌───────┐     .⌒.    └─────Ⓧ────────────┘
+│   n   │───>( > )
+└───────┘     `-'
+
+
+; Controller:
+
+               ┌───────┐       ┌───────┐
+        ┌──────│  p<-1 │<──────│  c<-1 │<─────start
+        │      └───────┘       └───────┘
+        ^
+       ╱ ╲
+      ╱   ╲
+ ┌──〈  >  〉───no────┐
+ │    ╲   ╱           │
+ │     ╲ ╱        ┌───────┐
+ │      V         │  p<-m │
+ │      │         └───────┘
+ │      │             │
+ │     yes            │
+ │      │             │
+ │      │             │
+ │      v             │
+ │    done            v
+ │                ┌───────┐
+ └────────────────│  c<-a │
+                  └───────┘
+
